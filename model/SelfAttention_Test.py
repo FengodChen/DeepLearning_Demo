3aa4f8590c3d08fee1587f1235be039906acc4d4
FengodChen <a1176553293@outlook.com>
2021-08-05 23:11:23 +0800

    add SelfAttention Test

--- model/SelfAttention_Test.py
+++ model/SelfAttention_Test.py
@@ -6,60 +6,10 @@ from copy import copy
 
 from model.Pos_Encode import get_2dPE_matrix
 
+
 class MuiltHead_SelfAttention(nn.Module):
-    '''
-    See `Attention Is All You Need <https://arxiv.org/abs/1706.03762>`
-
-    Example::
-        >>> x = torch.rand(batch_size, input_dim, embed_num)
-        >>> Net = MuiltHead_SelfAttention(sa_num, embed_num, input_dim, output_dim)
-        >>> y = Net(x)
-
-    Man::
-        x.shape = (batch_size, embed_num, input_dim)
-        Wq.shape = (input_dim, inner_dim)
-        Wk.shape = (input_dim, inner_dim)
-        Wv.shape = (embed_num, outut_dim)
-        y.shape = (batch_size, embed_num, output_dim)
-
-    Algorithm::
-        Input: x
-
-        k = (x @ Wk) + bias_k 
-        q = (x @ Wq) + bias_q
-        a = softmax((k @ q.tranpose) / sqrt(inner_dim))
-        y = (x @ Wv) + bias_v
-
-        Output: y
-    '''
     def __init__(self, sa_num, embed_num, input_dim, output_dim, inner_dim=None, qkv_bias=True):
         super(MuiltHead_SelfAttention, self).__init__()
-        inner_dim = inner_dim if inner_dim is not None else input_dim
-        self.inner_dim = inner_dim
-        self.sa_num = sa_num
-        
-        self.Wq = nn.Linear(input_dim, inner_dim*sa_num, bias=qkv_bias)
-        self.Wk = nn.Linear(input_dim, inner_dim*sa_num, bias=qkv_bias)
-        self.Wv = nn.Linear(embed_num*sa_num, output_dim*sa_num, bias=qkv_bias)
-        self.div_kq = Rearrange('b embed_num (sa_num input_dim) -> b sa_num embed_num input_dim', sa_num=sa_num)
-        self.combine_a = Rearrange('b sa_num embed_num_h embed_num_w -> b embed_num_h (sa_num embed_num_w)', sa_num=sa_num)
-        self.softmax = nn.Softmax(dim=1)
-        self.concat = nn.Linear(output_dim*sa_num, output_dim)
-
-    
-    def forward(self, x):
-        (b, _, __) = x.shape
-        k = self.div_kq(self.Wk(x))
-        q = self.div_kq(self.Wq(x))
-        q = q.transpose(2, 3)
-        a = self.softmax((k @ q) / sqrt(self.inner_dim))
-        v = self.Wv(self.combine_a(a))
-        y = self.concat(v)
-        return y
-
-class MuiltHead_SelfAttention_Test(nn.Module):
-    def __init__(self, sa_num, embed_num, input_dim, output_dim, inner_dim=None, qkv_bias=True):
-        super(MuiltHead_SelfAttention_Test, self).__init__()
         inner_dim = inner_dim if inner_dim is not None else input_dim
         self.inner_dim = inner_dim
         self.sa_num = sa_num
--- test/testrun4.py
+++ test/testrun4.py
@@ -1,7 +1,6 @@
 from torch.utils.data.dataloader import DataLoader
 from data.Datasets import cifar_dataset, mnist_dataset
-from model.SelfAttention import ViT
-#from model.SelfAttention_Test import ViT
+from model.ViT_Pytorch import ViT
 from data.Trainer import Trainer
 from data.Logger import ViT_Logger
 import torch
@@ -10,9 +9,9 @@ dev = torch.device("cuda:0")
 #dev = torch.device("cpu")
 dataset = cifar_dataset("datasets", True)
 dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
-net = ViT((32, 32), (2, 2), 16, 10, img_channel=3, dev=dev, sa_num=16, msa_num=6).to(dev)
+net = ViT(image_size=32, patch_size=2, dim=16, num_classes=10, channels=3, heads=16, depth=6, mlp_dim=16, dim_head=16).to(dev)
 loss = torch.nn.CrossEntropyLoss()
 opt = torch.optim.Adam(net.parameters(), lr=3e-4)
-logger = ViT_Logger("save/test1", net)
+logger = ViT_Logger("save/test2", net)
 
 trainer = Trainer(net, dataloader, loss, opt, dev, logger, 1)
